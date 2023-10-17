53fdfde976cb724c3525464412d395bb21f61497
smdick1995 <smdick1995@gmail.com>
2023-10-17 11:51:15 +0800

    build: tengoods.go finished

--- tengoods/tengoods.go
+++ tengoods/tengoods.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"log"
 	"os"
 
@@ -12,16 +13,26 @@ type Good struct {
 	Image string
 	Name  string
 	Price string
+	Store string
+	Left  string
 }
 
 var (
-	url string = "https://www.tengoods.com.tw/"
+	url   string = "https://www.tengoods.com.tw/"
+	goods []Good
 )
 
 func main() {
 	c := colly.NewCollector()
-	c.OnHTML("div.home-proitem-container", func(e *colly.HTMLElement) {
-
+	c.OnHTML(".home-proitem-container", func(e *colly.HTMLElement) {
+		good := Good{}
+		good.Url = e.ChildAttr("a", "href")
+		good.Image = e.ChildAttr("img", "data-src")
+		good.Name = e.ChildText(".home-proitem-item.home-proitem-pro")
+		good.Store = e.ChildText(".home-proitem-item.home-proitem-shop")
+		good.Price = e.ChildText(".home-proitem-item.home-proitem-price")
+		good.Left = e.ChildText(".home-proitem-item.home-proitem-qty")
+		goods = append(goods, good)
 	})
 	c.OnRequest(func(r *colly.Request) {
 		log.Println("Visiting", r.URL)
@@ -33,4 +44,23 @@ func main() {
 		log.Fatal(`Create csv file failed.`)
 	}
 	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	headers := []string{
+		"url", "image", "name", "store", "price", "left",
+	}
+	writer.Write(headers)
+
+	for _, good := range goods {
+		record := []string{
+			good.Url,
+			good.Image,
+			good.Name,
+			good.Store,
+			good.Price,
+			good.Left,
+		}
+		writer.Write(record)
+	}
+	defer writer.Flush()
 }
