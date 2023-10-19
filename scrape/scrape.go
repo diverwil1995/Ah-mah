36635356e93d1af3bfa426514d15d6f37fcd3f12
smdick1995 <smdick1995@gmail.com>
2023-10-19 16:21:18 +0800

    refactor: improve scrape.go download image

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"encoding/csv"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/gocolly/colly"
 )
 
 type Good struct {
 	Url   string
-	Image string
+	Image string //TODO: Download image.
 	Name  string
 	Price string
 	Store string
@@ -18,28 +21,53 @@ type Good struct {
 }
 
 var (
-	url   string = "https://www.tengoods.com.tw/"
-	goods []Good
+	url          string = "https://www.tengoods.com.tw"
+	goods        []Good
+	imgDirectory = "./output/img"
 )
+
+func downloadImage(url, imgPath string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	file, err := os.Create(imgPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	c := colly.NewCollector()
 	c.OnHTML(".home-proitem-container", func(e *colly.HTMLElement) {
 		good := Good{}
 		good.Url = e.ChildAttr("a", "href")
-		good.Image = e.ChildAttr("img", "data-src")
+		good.Image = url + e.ChildAttr("img", "data-src")
 		good.Name = e.ChildText(".home-proitem-item.home-proitem-pro")
 		good.Store = e.ChildText(".home-proitem-item.home-proitem-shop")
 		good.Price = e.ChildText(".home-proitem-item.home-proitem-price")
 		good.Left = e.ChildText(".home-proitem-item.home-proitem-qty")
 		goods = append(goods, good)
+
+		imgPath := filepath.Join(imgDirectory, good.Name+".jpg")
+		err := downloadImage(good.Image, imgPath)
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
 	c.OnRequest(func(r *colly.Request) {
 		log.Println("Visiting", r.URL)
 	})
 	c.Visit(url)
 
-	file, err := os.Create("TenGoods_HomePageItems.csv")
+	file, err := os.Create("./output/TenGoods_HomePageItems.csv")
 	if err != nil {
 		log.Fatal(`Create csv file failed.`)
 	}
